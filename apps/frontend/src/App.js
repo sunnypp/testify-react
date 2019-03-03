5f63455799e2d996cc41eca462492eff2ba346ac
Sunny Pun <sunnyppsp@gmail.com>
2019-03-03 20:47:16 +0800

    Working with styling but no markdown yet

--- apps/frontend/src/App.js
+++ apps/frontend/src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
-import Test from './pages/Test'
+import Test from './pages/Test';
+import { Normalize } from 'styled-normalize';
+import { createGlobalStyle } from 'styled-components';
 
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    overflow: hidden;
+  }
+`
 class App extends Component {
   render() {
     return (
       <>
+        <Normalize/>
+        <GlobalStyle/>
         <Test/>
       </>
     );
--- apps/frontend/src/pages/Test.js
+++ apps/frontend/src/pages/Test.js
@@ -3,8 +3,110 @@ import React, {
   useState,
 } from 'react';
 import styled from 'styled-components';
+import {
+  color,
+  fontSize,
+  space,
+  width,
+} from 'styled-system';
 import loadRows from '../utils/GoogleSheet.js';
 import { isEmpty } from '@testify-react/validator';
+
+const Header = styled.header`
+  ${space}
+
+  ${color}
+  ${fontSize}
+
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+
+  width: 100%;
+
+`
+
+const Input = styled.input`
+  appearance: none;
+  border: none;
+
+  ${width}
+
+  ${fontSize}
+
+  background: transparent;
+  color: rgb(200,200,190);
+`
+
+const Label = styled.div`
+  ${fontSize}
+
+  flex: 1;
+  padding-right: 30px;
+
+  color: white;
+  text-align: right;
+`
+
+const Text = styled.div`
+  ${space}
+`
+
+const Container = styled.div`
+  height: 100vh;
+
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  background: linear-gradient(90deg, rgba(243, 72, 104,1) 20.5625%,rgba(242, 71, 104,1) 20.5625%,rgba(158, 0, 236,1) 80.5625%);
+  font-family: Arial, sans-serif;
+`
+
+const Flex = styled.div`
+  display: flex;
+  flex-direction: row;
+
+  align-items: center;
+
+  flex: 1;
+`
+
+const Button = styled.button`
+  border: none;
+
+  ${fontSize}
+  ${space}
+  width: 100%;
+
+  background: rgba(255,255,255,.4);
+  border-radius: 20px;
+  color: white;
+  text-transform: uppercase;
+  
+  :disabled {
+    color: #999;
+  }
+`
+
+const QuestionArea = styled.section`
+  ${fontSize}
+  ${space}
+
+  display: flex;
+  flex-direction: column;
+  // align-items: center;
+  // justify-content: center;
+
+  color: white;
+  flex: 1;
+  text-align: center;
+  overflow: scroll;
+`
+
+const Center = styled.div`
+  margin: auto;
+`
 
 export default props => {
   const [ sheet, setSheet ] = useState('12vrGNS-M8cwCXOPhhC64fORW-xRfVfmGsxW7Q3EZvpk');
@@ -43,14 +145,26 @@ export default props => {
   }, [ sheet, page ] );
 
   return (
-    <>
-      <input onChange={ e => setSheet(e.target.value) } value={sheet}/>
-      <input onChange={ e => setPage(e.target.value) } value={page}/>
-      <div>{ !!answering.length && answering[0].question }</div>
-      { showAnswer && <div>{ !!answering.length && answering[0].answer }</div> }
-      <button onClick={ _ => setShowAnswer(true) } disabled={showAnswer}>Show Answer</button>
-      <button onClick={ _ => { fetchQuestion(questions); setShowAnswer(false); } }>Next</button>
-    </>
+    <Container>
+      <Header p={[3,4]}>
+        <Flex>
+          <Label>Sheet</Label>
+          <Input color={'rgb(100,100,80)'} fontSize={[3,4,5]} onChange={ e => setSheet(e.target.value) } value={sheet}/>
+        </Flex>
+        <Flex>
+          <Label>Page</Label>
+          <Input color={'rgb(100,100,80)'} fontSize={[3,4,5]} onChange={ e => setPage(e.target.value) } value={page}/>
+        </Flex>
+      </Header>
+      <QuestionArea p={[3,4]} fontSize={[4,5,6]}>
+        <Center>
+          <Text my={3}>{ !!answering.length && answering[0].question }</Text>
+          <Text>{ showAnswer && !!answering.length && answering[0].answer }</Text> 
+        </Center>
+      </QuestionArea>
+      <Button fontSize={[3,4,5]} my={[1,2]} p={[3,4]} onClick={ _ => setShowAnswer(true) } disabled={showAnswer}>Show Answer</Button>
+      <Button fontSize={[3,4,5]} my={[1,2]} p={[3,4]} onClick={ _ => { fetchQuestion(questions); setShowAnswer(false); } }>Next</Button>
+    </Container>
   );
 }
 
